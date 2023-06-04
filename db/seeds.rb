bc2b29510b98dde3423b31d5049bce94de46197e
Gabriel sena <gabrieloddone@id.uff.br>
2023-06-04 18:47:42 -0300

    Fixing seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Categoria.create(name: "Condominio", description: "Gasto de condominio")
-Categoria.create(name: "Conta de Energia", description: "Gasto de energia/luz")
-Categoria.create(name: "Comida", description: "Refeição/Delivery")
+Categoria.find_or_create_by(name: "Condominio", description: "Gasto de condominio")
+Categoria.find_or_create_by(name: "Conta de Energia", description: "Gasto de energia/luz")
+Categoria.find_or_create_by(name: "Comida", description: "Refeição/Delivery")
+Categoria.find_or_create_by(name: "Services", description: "General Services")
+Categoria.find_or_create_by(name: "Credit Card", description: "Credit card spendings")
