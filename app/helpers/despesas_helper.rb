5e78cb26c970a11f621115156e2283d213fa3e6b
Gabriel sena <gabrieloddone@id.uff.br>
2023-08-19 00:41:14 -0300

    Formatting decimal values

--- app/helpers/despesas_helper.rb
+++ app/helpers/despesas_helper.rb
@@ -10,4 +10,8 @@ module DespesasHelper
   def formatted_date(date)
     date.strftime('%b %e, %Y')
   end
+
+  def formatted_decimal(value)
+    number_with_precision(value, precision: 2, delimiter: I18n.t('number.format.delimiter'), separator: I18n.t('number.format.separator'))
+  end
 end
