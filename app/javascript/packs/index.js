3a3022aed3f8ab3bbcd9d721fa44524a6d86fb11
Gabriel sena <gabrieloddone@id.uff.br>
2023-11-04 22:31:36 -0300

    Adding panel to future expenses

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var secondToLastMonth = $('.toggle-month').eq(-2).data('month');
 
   $('.panel').hide();
-  // $('.abc').show();
+  $('.delayed-job-panel').show();
   $('.panel[data-month="' + latestMonth + '"]').show();
   $('.panel[data-month="' + secondToLastMonth + '"]').show();
 
