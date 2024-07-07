72126d0a1f607e91c39c099fed2338deec881106
Gabriel sena <gabrieloddone@id.uff.br>
2024-07-07 02:14:23 -0300

    Formatting and translating category select attribute values

--- app/helpers/despesas_helper.rb
+++ app/helpers/despesas_helper.rb
@@ -14,4 +14,8 @@ module DespesasHelper
   def formatted_decimal(value)
     number_with_precision(value, precision: 2, delimiter: I18n.t('number.format.delimiter'), separator: I18n.t('number.format.separator'))
   end
+
+  def category_options
+    Categoria.all.map { |c| [Categoria.human_attribute_value(:name, c.name), c.id] }
+  end
 end
