fee53a849940da0c34bfc6ac79eb0e76c0295215
Gabriel sena <gabrieloddone@id.uff.br>
2024-10-21 06:54:28 -0300

    User setting language switching

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,4 +2,13 @@ class SettingsController < ApplicationController
   def index
     @user = current_user
   end
+
+  def change_locale
+    locale = params[:locale]
+    
+    session[:locale] = locale
+    I18n.locale = locale
+    
+    redirect_back(fallback_location: root_path)
+  end
 end
