ce4e47cb2f5fb38cb30c96d0429a87c9398b788d
Gabriel sena <gabrieloddone@id.uff.br>
2024-11-22 23:50:44 -0300

    Refactor actions and adding translations

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -12,7 +12,7 @@ class SettingsController < ApplicationController
     @category.user_id = current_user.id
 
     if @category.save
-      redirect_to categorias_settings_path, notice: "Category created successfully."
+      redirect_to categorias_settings_path, notice: t('settings.category_created')
     else
       render :new
     end
@@ -22,12 +22,12 @@ class SettingsController < ApplicationController
     category_ids = params[:category_ids]
     current_user.user_configuration.update(categories: category_ids.map(&:to_i))
 
-    redirect_to categorias_settings_path, notice: "Categories updated successfully."
+    redirect_to categorias_settings_path, notice: t('settings.category_updated')
   end
 
   def change_locale
     locale = params[:locale]
-    user_config = current_user.user_configuration || current_user.build_user_configuration
+    user_config = current_user.user_configuration
     user_config.locale = locale
     user_config.save
 
@@ -38,28 +38,19 @@ class SettingsController < ApplicationController
   end
 
   def change_panel_color
-    panel_color = params[:panel_color]
-    user_config = current_user.user_configuration || current_user.build_user_configuration
-    user_config.panel_color = panel_color
-    user_config.save
+    current_user.user_configuration.update(panel_color: params[:panel_color])
 
     redirect_back(fallback_location: root_path)
   end
 
   def change_currency
-    currency = params[:currency]
-    user_config = current_user.user_configuration || current_user.build_user_configuration
-    user_config.currency = currency
-    user_config.save
+    current_user.user_configuration.update(currency: params[:currency])
 
     redirect_back(fallback_location: root_path)
   end
 
   def change_ordering
-    order = params[:order]
-    user_config = current_user.user_configuration || current_user.build_user_configuration
-    user_config.order = order
-    user_config.save
+    current_user.user_configuration.update(order: params[:order])
 
     redirect_back(fallback_location: root_path)
   end
